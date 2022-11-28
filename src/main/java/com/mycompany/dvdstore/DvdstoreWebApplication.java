fd9da46e0e8c1235c23a1e48408f6f0da4384258
Gildas Rouault <g.rouault@groupe-pomona.fr>
2022-11-28 11:21:07 +0100

    exercice-29 : première connexion à la base

--- src/main/java/com/mycompany/dvdstore/DvdstoreWebApplication.java
+++ src/main/java/com/mycompany/dvdstore/DvdstoreWebApplication.java
@@ -1,13 +1,39 @@
 package com.mycompany.dvdstore;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DvdstoreWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DvdstoreWebApplication.class, args);
+		ApplicationContext context = SpringApplication.run(DvdstoreWebApplication.class, args);
+		DataSource ds = context.getBean(DataSource.class);
+		Connection conn = null;
+		try {
+			conn = ds.getConnection();
+			System.out.println("sucess connect");
+			ResultSet rs = conn.createStatement().executeQuery("SELECT title, genre, description FROM movie");
+			while(rs.next()){
+				System.out.println(rs.getString("title") + " | " + rs.getString("genre") + " | " + rs.getString("description"));
+			}
+		} catch (SQLException e){
+			e.printStackTrace();
+		} finally {
+			try{
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e){
+				e.printStackTrace();
+			}
+
+		}
 	}
 
 }
